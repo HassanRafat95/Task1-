4aee3cbeb8dd964133e16e61aa0786922bd5b851
HASSN RAFAT <hassanrafat95@gmail.com>
2021-08-01 14:47:39 +0200

    edit signup

---
+++ edit_signup/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name":"edit sign up",
+    "author":"hassan raafat ",
+    "summary":"edit sign up form ",
+    "depends":['auth_signup'],
+    "data":
+        [
+            "security/ir.model.access.csv",
+            "views/signup_view.xml"
+
+        ],
+}
